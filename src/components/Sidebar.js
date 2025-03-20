4e93ad9aea54833afa538495e0217abea28d9b71
fabiobnm <ffabiobnm@gmail.com>
2025-03-20 16:59:47 +0100

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -52,7 +52,7 @@ const Sidebar = () => {
         {/* Guido Borso a sinistra */}
         <li style={styles.left}>
           <Link className='vociMenuHeader' id='guidoBold' href="/" style={router.pathname === '/' ? styles.linkHome : styles.linkHome}>
-            <img style={{width:'80px'}} src='https://guidoborso.com/new/img/GuidoBorso.png'/>
+            <img style={{width:'80px'}} src='./GuidoBorsologo.png'/>
           </Link>
         </li>
 
