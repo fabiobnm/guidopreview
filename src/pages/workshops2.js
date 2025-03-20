f59649e470e6628b42f3c68449436c19d1c60a99
fabiobnm <ffabiobnm@gmail.com>
2025-03-20 19:56:50 +0100

    Add files via upload

---
+++ src/pages/workshops2.js
@@ -0,0 +1,51 @@
+import { useQuery } from '@apollo/client';
+import client from '../lib/apolloClient';
+import { GET_POSTSEducNew } from '../lib/queries';
+import Sidebar from '../components/Sidebar'; 
+import { useState } from 'react';
+
+export default function Home() {
+  const { loading, error, data } = useQuery(GET_POSTSEducNew, { client });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    alert('err');
+    console.error('Errore nella query:', error.message);
+    console.error('Dettagli dell\'errore:', error.graphQLErrors);
+    console.error('Dettagli della risposta:', error.networkError);
+    return <p>Error: {error.message}</p>;
+  }
+
+  console.log('Dati ricevuti:', data.workshops[0]);
+
+  return (
+    <main>
+      <div className='textWorkshops' dangerouslySetInnerHTML={{ __html: data.workshops[0].text.html }}></div>
+      
+      <div className="educational">
+        {data.workshops[0].educational.map((progetto, index) => (
+          <ImageHover key={index} progetto={progetto} />
+        ))}
+      </div>
+      
+      <Sidebar />
+    </main>
+  );
+}
+
+// Componente separato per la gestione dell'hover
+const ImageHover = ({ progetto }) => {
+  const [imageSrc, setImageSrc] = useState(progetto.copertina.url);
+
+  return (
+    <div>
+      <img 
+        style={{ height: '48vH',maxWidth:'30vW', margin: 'auto', transition: 'opacity 0.3s ease-in-out' }} 
+        src={imageSrc} 
+        alt={progetto.nome}
+        onMouseEnter={() => setImageSrc(progetto.retro.url)}  // Cambia immagine all'hover
+        onMouseLeave={() => setImageSrc(progetto.copertina.url)} // Ritorna all'immagine originale
+      />
+    </div>
+  );
+};
